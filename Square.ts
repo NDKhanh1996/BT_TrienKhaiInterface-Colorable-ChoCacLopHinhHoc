4644279ed921bc86a6788d7cf020ae3a97bf95cf
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-04-10 11:51:17 +0700

    add

--- Square.ts
+++ Square.ts
@@ -1,6 +1,6 @@
 import {Colorable} from "./Colorable";
 
-class Square implements Colorable{
+class Square implements Colorable {
     private _side: number;
     private _color: boolean;
 
@@ -28,24 +28,27 @@ class Square implements Colorable{
     howToColor(): string {
         return "Color all four sides"
     }
-    getArea(): number{
+
+    getArea(): number {
         return this._side * this._side
     }
-    showInfo():string{
-        if (this._color === true){
+
+    showInfo(): string {
+        if (this._color === true) {
             return `S = ${this.getArea()} and ${this.howToColor()}`
-        }
-        else {
+        } else {
             return `S = ${this.getArea()}`
         }
     }
 }
-let square1 = new Square( 10, true);
-let square2 = new Square( 5, false);
-let square3 = new Square( 8, false);
-let square4 = new Square( 3, true);
 
-let arr = [square1,square2,square3,square4]
+let square1: Square = new Square(10, true);
+let square2: Square = new Square(5, false);
+let square3: Square = new Square(8, false);
+let square4: Square = new Square(3, true);
+
+let arr: Square[] = [square1, square2, square3, square4]
 
 arr.forEach((i) => {
-    console.log(i.showInfo())})
+    console.log(i.showInfo())
+})
